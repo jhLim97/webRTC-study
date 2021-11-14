6a561f9d84a4ae5f7a74fd2b1e101f1bfd843113
jhLim97 <wnsgur2749@naver.com>
2021-11-14 23:08:11 +0900

    feat: webSocket open, message, close, send 이벤트 실습

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,2 +1,20 @@
 // socket : 연결된 서버
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener("open", () => {
+  console.log("Connected to Server ✅");
+});
+
+socket.addEventListener("message", (message) => {
+  console.log("New message: ", message.data);
+});
+
+socket.addEventListener("close", () => {
+  console.log("Disconnected from Server ❌");
+});
+
+setTimeout(() => {
+  socket.send("hello from the browser!");
+}, 10000);
+
+socket.addEventListener('')
